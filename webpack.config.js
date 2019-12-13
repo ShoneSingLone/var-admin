7e46278cd39335dc0b0bc56c50c88b7683b58b1b
ShoneSingLone <ShoneSingLone@gmail.com>
2019-12-13 13:10:39 +0800

    koa-hmr-webpack-master init

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,46 @@
-const path = require("path");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const webpackBaseConfig = require('./config/webpack.base.config')
+const paths = require('./config/paths')
 
-module.exports = {
-    mode: "production",
-    entry: "./workspace/_main.js",
-    watch: true,
-    module: {
-        rules: [
-            /* { test: /\.scss$/, use: (() => { let loader = ExtractTextPlugin .extract({ fallback: "style-loader", use: [ "css-loader", { loader: "postcss-loader", options: { sourceMap: false, config: { path: "postcss.config.js" } } }, "sass-loader" ] }); return loader; })() }, */
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.styl$/,
-                use: (() => {
-                    let loader = ExtractTextPlugin
-                        .extract({
-                            fallback: "style-loader",
-                            use: [
-                                "css-loader",
-                                {
-                                    loader: "postcss-loader",
-                                    options: {
-                                        sourceMap: false,
-                                        config: {
-                                            path: "postcss.config.js"
-                                        }
-                                    }
-                                },
-                                "stylus-loader"
-                            ]
-                        });
-                    return loader;
-                })()
-            },
-            {
-                test: /\.png$/,
-                loader: "file-loader"
-            }
-        ],
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new ExtractTextPlugin("main.css"),
+module.exports = merge(webpackBaseConfig, {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: [
+    'babel-polyfill',
+    './index',
+  ],
+  output: {
+    path: paths.output,
+    filename: 'static/js/[name].[hash:8].js',
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        parallel: true,
+        cache: true,
+      }),
     ],
-    output: {
-        path: path.resolve(__dirname, "./public/statics/css"),
-        filename: "main.js"
-    }
-};
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'main',
+          chunks: 'all',
+          minChunks: 2
+        },
+      },
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+        BABEL_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
+})
