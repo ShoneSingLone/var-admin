8652a1ad0f810f06beeef2ab20ce97679befe19b
ShoneSingLone <ShoneSingLone@gmail.com>
2020-04-11 23:38:12 +0800

    push hooks

--- config/pre/changeversion.js
+++ config/pre/changeversion.js
@@ -1,1 +1,6 @@
-console.log("changeversion");
+const fs = require("fs-extra");
+const path = require("path");
+let str = fs.readFileSync(path.resolve(__dirname, "../..", "public/static/js/main.sentry.js"), "utf-8");
+const strnew = `STATIC_RES_VERSION: IS_DEV ? Date.now() : "${Date.now()}",`;
+const regExp = /STATIC_RES_VERSION: IS_DEV \? Date.now\(\) : "([^"].*)",/;
+fs.writeFileSync(path.resolve(__dirname, "../..", "public/static/js/main.sentry.js"), str.replace(regExp, strnew));
