e26d6496bfacfd734f43a8766f923ab9aac166f4
ShoneSingLone <ShoneSingLone@gmail.com>
2020-11-25 11:03:57 +0800

    flip complate

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 Vue\Angular\React
 
 - [readme](https://shonesinglone.github.io/var-admin)
-  - [github](https://shonesinglone.github.io/var-admin/public/login.html)
-  - [gitee 免费版不能自动部署](https://shonesinglone.gitee.io/var-admin/login.html)
-  - [coding 能自动部署，但是访问地址是随机码 jz0a27.coding-pages.com](https://jz0a27.coding-pages.com/public/login.html)
+  - [github](https://shonesinglone.github.io/var-admin/public)
+  - [gitee 免费版不能自动部署](https://shonesinglone.gitee.io/var-admin)
+  - [coding 能自动部署，但是访问地址是随机码 jz0a27.coding-pages.com](https://jz0a27.coding-pages.com/public)
 
 基于SystemJS，使用VueJS编写基座的微前端Demo。
 如果是内部使用的中后台系统，第一次加载资源的时间漫长是可以忍受的，而且内部网络速度按常理是比较快的。
--- public/static/module/demo/iframe/PageReact/FLIP.tsx
+++ public/static/module/demo/iframe/PageReact/FLIP.tsx
@@ -1,11 +1,10 @@
-(({ React, ReactDOM }) => {
+(({ React, ReactDOM, $ }) => {
   const { useState, useEffect, useRef } = React;
   const { createPortal } = ReactDOM;
 
 
   /*  */
   const bodyContainer = document.body;
-
 
   function generateData(size) {
     function getSize() {
@@ -38,33 +37,51 @@
   let currentItem = {};
   let firstState = null;
   let lastState = null;
+  /* 缓存还原时再用 */
+  let cacheTransform = "";
 
   function FLIP() {
     const [showModal, setShowModal] = useState(false);
     const [modalItemStyle, setModalItemStyle] = useState({});
     const previewRef = useRef();
+    function play() {
+
+      cacheTransform = previewRef.current.style.transform;
+      previewRef.current.style.transform = "unset";
+    }
+    function turn({ setShowModal }) {
+      const hiddenModal = () => {
+        setShowModal(false);
+        cacheTransform = "";
+      };
+      previewRef.current.style.transform = cacheTransform;
+      setTimeout(hiddenModal, 1200);
+    }
+    cacheTransform = "";
+
     useEffect(() => {
       if (showModal) {
-        bodyContainer.style.overflow = "hidden"
         /*###2 Last：执行一段代码，让元素发生相应的变化，并记录元素在动画最后状态的位置和尺寸，即动画结束之后那一刻元素的位置和尺寸信息 */
         lastState = previewRef.current.getBoundingClientRect();
-        debugger;
+        /* Invert：计算元素第一个位置（First）和最后一个位置（Last）之间的位置变化（如果需要，还可以计算两个状态之间的尺寸大小的变化），然后使用这些数字做一定的计算，让元素进行移动（通过 transform来改变元素的位置和尺寸），从而创建它位于第一个位置（初始位置）的一个错觉
+        即，一上来直接让元素处于动画的结束状态，然后使用 transform属性将元素反转回动画的开始状态（这个状态的信息在 First步骤就拿到了） */
+        /* 缩放比例 */
         const scaleValue = firstState.width / lastState.width;
-        const translateX = firstState.width - lastState.width;
-        const translateY = firstState.height - lastState.height;
+        /* 偏移距离 */
+        const translateX = firstState.left - lastState.left;
+        const translateY = firstState.top - lastState.top;
+
         const style = Object.assign({
           transform: `
-            scale(${scaleValue})
-            translateX(${translateX}px) 
-            translateY(${translateY}px)
-          `
-        }, makestyle(currentItem))
-        /* Invert：计算元素第一个位置（First）和最后一个位置（Last）之间的位置变化（如果需要，还可以计算两个状态之间的尺寸大小的变化），然后使用这些数字做一定的计算，让元素进行移动（通过 transform来改变元素的位置和尺寸），从而创建它位于第一个位置（初始位置）的一个错觉
-            即，一上来直接让元素处于动画的结束状态，然后使用 transform属性将元素反转回动画的开始状态（这个状态的信息在 First步骤就拿到了） */
-        /* Play：将元素反转（假装在first位置），我们可以把 transform设置为 none，因为失去了 transform的约束，所以元素肯定会往本该在的位置（即动画结束时的那个状态）进行移动，也就是last的位置，如果给元素加上 transition的属性，那么这个过程自然也就是以一种动画的形式发生了 */
+                    translate(${translateX}px,${translateY}px) 
+                    scale(${scaleValue})
+                    `
+        }, makestyle(currentItem));
+
         setModalItemStyle(style);
-      } else {
-        bodyContainer.style.overflow = "auto"
+        /* Play：将元素反转（假装在first位置），我们可以把 transform设置为 none，因为失去了 transform的约束，所以元素肯定会往本该在的位置（即动画结束时的那个状态）进行移动，也就是last的位置，如果给元素加上 transition的属性，那么这个过程自然也就是以一种动画的形式发生了 */
+
+        setTimeout(play, 90);
       }
     }, [showModal]);
 
@@ -84,7 +101,6 @@
       }
     }
 
-
     const generateImg = (pictures, handlePreviewPicture) => pictures.map((item, index) => {
       return (<li
         key={index}
@@ -97,25 +113,20 @@
     });
 
     function Modal(props) {
-      return createPortal(<div className="modal-container" onClick={props.onClick}>{props.children}</div>, bodyContainer);
+      return createPortal(<div className="modal-container" onClick={props.onClick} onWheel={event => event.stopPropagation()}>{props.children}</div>, bodyContainer);
     }
 
     function generateModal({ showModal, setShowModal }) {
       if (showModal) {
         return (
-          <Modal onClick={() => setShowModal(false)}>
-            <div style={modalItemStyle} className="pic-item modal-item" ref={previewRef} >
-              <pre>{JSON.stringify(firstState, null, 1)}</pre>
-              <pre>{JSON.stringify(lastState, null, 1)}</pre>
-            </div>
-          </Modal>
+          <Modal onClick={() => turn({ setShowModal })} >
+            <div className="pic-item modal-item" style={modalItemStyle} ref={previewRef} />
+          </Modal >
         );
       } else {
         return "";
       }
     }
-
-
     return <>
       <ul className="pic-list">{generateImg(pictures, handlePreviewPicture)}</ul>
       {generateModal({ showModal, setShowModal })}
@@ -123,7 +134,6 @@
   }
 
   const element = (() => {
-
     return (
       <>
         <fieldset style={{ overflow: 'hidden' }}>
