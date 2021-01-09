c36a0db17f3c91c1dd02f9ea177c45ca69be2169
ShoneSingLone <ShoneSingLone@gmail.com>
2021-01-09 20:32:01 +0800

    add img

--- public/projects/interview/main.js
+++ public/projects/interview/main.js
@@ -4,25 +4,38 @@
         data() {
             return {
                 urls: [{
-                        url: "nav",
+                        url: "nav.png",
+                        question: [
+                            '使用的第三方库有哪些',
+                            '多边形如何制作'
+                        ]
                     },
                     {
-                        url: "login",
+                        url: "login.png",
+                        question: [
+                            '布局方案',
+                            '组件划分',
+                            '预计以后会修改的部分'
+                        ]
+
                     },
                     {
-                        url: "首页",
+                        url: "首页.png",
                     },
                     {
-                        url: "下钻",
+                        url: "下钻.png",
                     },
                     {
-                        url: "info",
+                        url: "info.png",
                     },
                     {
-                        url: "list",
+                        url: "et.gif",
                     },
                     {
-                        url: "detail",
+                        url: "list.png",
+                    },
+                    {
+                        url: "detail.png",
                     }
                 ]
             }
